255cc86056c23625fa748918e716d40adb7b8b72
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2021-10-26 12:58:50 -0300

    Adicionando novo teste

--- seven-seg/src/app/app.component.ts
+++ seven-seg/src/app/app.component.ts
@@ -17,6 +17,8 @@ export class AppComponent  {
   char8: string = ''
   char9: string = ''
   char10: string = ''
+  char11: string = ''
+  char12: string = ''
   i: number = 0
 
   text: string = "Leonam Teixeira de Vasconcelos"
@@ -65,19 +67,35 @@ export class AppComponent  {
       return n % this.text.length
     }
 
+    // setInterval (() => {
+    //   this.char1 = this.text.charAt(index( this.i ))
+    //   this.char2 = this.text.charAt(index( this.i + 1 ))
+    //   this.char3 = this.text.charAt(index( this.i + 2 ))
+    //   this.char4 = this.text.charAt(index( this.i + 3 ))
+    //   this.char5 = this.text.charAt(index( this.i + 4 ))
+    //   this.char6 = this.text.charAt(index( this.i + 5 ))
+    //   this.char7 = this.text.charAt(index( this.i + 6 ))
+    //   this.char8 = this.text.charAt(index( this.i + 7 ))
+    //   this.char9 = this.text.charAt(index( this.i + 8 ))
+    //   this.char10 = this.text.charAt(index( this.i + 9 ))
+    //   this.i = ( this.i + 1 ) % this.text.length
+    // }, 600)
+
     setInterval (() => {
-      this.char1 = this.text.charAt(index( this.i ))
-      this.char2 = this.text.charAt(index( this.i + 1 ))
-      this.char3 = this.text.charAt(index( this.i + 2 ))
-      this.char4 = this.text.charAt(index( this.i + 3 ))
-      this.char5 = this.text.charAt(index( this.i + 4 ))
-      this.char6 = this.text.charAt(index( this.i + 5 ))
-      this.char7 = this.text.charAt(index( this.i + 6 ))
-      this.char8 = this.text.charAt(index( this.i + 7 ))
-      this.char9 = this.text.charAt(index( this.i + 8 ))
-      this.char10 = this.text.charAt(index( this.i + 9 ))
-      this.i = ( this.i + 1 ) % this.text.length
-    }, 600)
+      let date = Math.round(new Date().getTime()/1000)
+      this.char1  = ( date / 10 ).toString()
+      this.char2  = ( date / 100 ).toString()
+      this.char3  = ( date / 1000 ).toString()
+      this.char4  = ( date / 10000 ).toString()
+      this.char5  = ( date / 100000 ).toString()
+      this.char6  = ( date / 1000000 ).toString()
+      this.char7  = ( date / 10000000 ).toString()
+      this.char8  = ( date / 100000000 ).toString()
+      this.char9  = ( date / 1000000000 ).toString()
+      this.char10 = ( date / 10000000000 ).toString()
+      this.char11 = ( date / 100000000000 ).toString()
+      this.char12 = ( date / 1000000000000 ).toString()
+    }, 1000)
   }
 
 }
